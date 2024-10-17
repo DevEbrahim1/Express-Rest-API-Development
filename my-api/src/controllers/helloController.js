bc92a11f522a1b1224231bc7518a483f2dc58386
DevEbrahim1 <iamebrahimkholil@gmail.com>
2024-10-17 18:35:29 +0600

    update

--- my-api/src/controllers/helloController.js
+++ my-api/src/controllers/helloController.js
@@ -1,5 +1,10 @@
-exports.Hello=(req,res)=>{
+exports.HelloGet=(req,res)=>{
 
-res.status(200).json({status:"success",data:"Hello this is my first express rest api"})
+res.status(200).json({status:"success",data:"Hello Get"})
 
 }
+exports.HelloPost=(req,res)=>{
+
+    res.status(200).json({status:"success",data:"Hello Post"})
+    
+    }
--- my-api/src/routes/api.js
+++ my-api/src/routes/api.js
@@ -4,9 +4,9 @@ const router= express.Router();
 
 
 //this is my first get routing 
-router.get("/hello-get",helloController.Hello)
+router.get("/hello-get",helloController.HelloGet)
 
 //this is my first post routing 
-router.post("/hello-post",helloController.Hello)
+router.post("/hello-post",helloController.HelloPost)
 
 module.exports=router;
